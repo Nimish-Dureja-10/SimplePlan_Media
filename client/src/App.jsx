c66d067d5c4dd2322c9163a5530d62a2ea9cdce7
Nimish-Dureja-10 <durejanimish14@gmail.com>
2025-01-07 17:09:20 +0530

    Initial application setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <h1 className="text-3xl text-red-500">Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
